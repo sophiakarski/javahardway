d0002ceb5a8317740d2b8d9970b8364d5d5acb38
Sophia Karski <sophiakarski@gmail.com>
2022-02-02 14:35:47 +0000

    Add all files so far

---
+++ FirstProg.java
@@ -0,0 +1,1 @@
+// An Important Messagepublic class FirstProg {	public static void main( String[] args ) {		System.out.println( "I, Sophia Karski, am determined to learn how to code." );		System.out.println( "Today's date is Friday 21st January 2022." );	}}
